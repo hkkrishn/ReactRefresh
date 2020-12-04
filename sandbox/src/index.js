529969adb4bd5977888113c0273b0320240546fb
Harirkishnan Kuppusamykrishnan <hkkrishn563@gmail.com>
2020-12-03 21:33:00 -0800

    first commit

---
+++ sandbox/src/index.js
@@ -0,0 +1,27 @@
+//Author:Harikrishnan
+//Project:Sandbox refresh
+//Date: 12/02/2020
+
+// Import the react and react dom libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+// Create a react component
+// A react component can be a function or a class
+// the output is to produce HTML to the user and handles feedback from the user -- Using JSX and handlers
+
+// The three tenes of component creation 
+// 1 component Nesting
+// 2. component Reusability
+// 3. Component Configuration - We should be able to configure a component when it is created
+
+
+ReactDOM.render(
+    <App/>, 
+    document.querySelector('#root')
+);
+
+
+
+// Take the react component and show it on the screen
